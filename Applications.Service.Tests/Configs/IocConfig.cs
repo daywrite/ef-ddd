c4e2c011b57ebfb6b214c79980ce50a46eaa34e6
daywrite@yeah.net <daywrite@yeah.net>
2015-07-09 18:13:24 +0800

    测试

---
+++ Applications.Service.Tests/Configs/IocConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Applications.Datas.Ef;
+using Autofac;
+
+namespace Applications.Service.Tests.Configs
+{
+    /// <summary>
+    /// 应用程序Ioc配置
+    /// </summary>
+    public class IocConfig : OBear.DI.Autofac.ConfigBase
+    {
+        /// <summary>
+        /// 加载配置
+        /// </summary>
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ApplicationUnitOfWork>().As<IApplicationUnitOfWork>().InstancePerLifetimeScope();
+        }
+    }
+}
